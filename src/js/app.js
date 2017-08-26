dd08211759ee36ae6711e019a94bc45b523c4765
savvala <savva.l@hotmail.com>
2017-08-26 13:41:49 +0100

    created collision function

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,30 @@
-console.log('oi');
+$(() => {
+
+  console.log('oi');
+
+  const $ball = $('#ball');
+  const $launch = $('#launch');
+  const $frame = $('.box');
+  const $target = $('.target');
+
+  function checkCollision () {
+    const ballPos = $ball.offset();
+    const targetPos = $target.offset();
+    const framePos = $frame.offset();
+
+    const ballRightPos = ballPos.left + $ball.width();
+    if(ballRightPos >= targetPos.left) {
+      $ball.stop();
+    }
+  }
+
+  $launch.click(function launch(){
+    console.log('here');
+    $ball.animate({ left: '2000px' }, {
+      progress: checkCollision,
+      duration: 2000,
+      easing: 'easeOutCubic'
+    });
+  });
+
+}); // closing tag for DOM content loaded - leave here
